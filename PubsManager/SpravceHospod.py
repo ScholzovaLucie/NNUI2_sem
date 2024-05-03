dfc1ecb7f11e3dfbda9b8adb612a8d9abb229f53
ScholzovaLucie <lucka.scholz@gmail.com>
2024-05-03 10:05:03 +0200

    vytvoření matice vzdáleností

--- PubsManager/SpravceHospod.py
+++ PubsManager/SpravceHospod.py
@@ -1,5 +1,7 @@
 import math
 
+import numpy as np
+
 
 class SpravceHospod:
     def __init__(self, hospody):
@@ -19,3 +21,17 @@ class SpravceHospod:
         r = 6371.0  # Poloměr Země v kilometrech
         vzdalenost = r * c
         return vzdalenost
+
+    def create_distance_matrix(self, pubs_data):
+        num_pubs = len(pubs_data)
+        distance_matrix = np.zeros((num_pubs, num_pubs))
+
+        for i in range(num_pubs):
+            for j in range(num_pubs):
+                if i != j:
+                    coord_i = np.array(pubs_data[i]['koordináty'])
+                    coord_j = np.array(pubs_data[j]['koordináty'])
+                    # Euklidovská vzdálenost mezi dvěma body
+                    distance_matrix[i][j] = np.linalg.norm(coord_i - coord_j)
+
+        return distance_matrix
