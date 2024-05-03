9925a217bd2a52e9ac268fbe89ddf60d3c1fb52d
ScholzovaLucie <lucka.scholz@gmail.com>
2024-05-03 10:04:38 +0200

    vytvoření analýz

--- AntColony/AnalyzaVysledku.py
+++ AntColony/AnalyzaVysledku.py
@@ -12,9 +12,20 @@ class AnalizaVysledku:
 
     def vykresli_graf(self):
         generace = range(1, len(self.vzdalenosti) + 1)
-        plt.plot(generace, self.vzdalenosti)
+        plt.figure(figsize=(10, 5))  # Nastavení velikosti grafu
+        plt.plot(generace, self.vzdalenosti, label='Vzdálenost')
+
+        # Najít minimum a jeho index
+        min_vzdalenost = min(self.vzdalenosti)
+        min_index = self.vzdalenosti.index(min_vzdalenost) + 1  # +1 protože indexace generací začíná od 1
+
+        # Zvýraznit minimum
+        plt.scatter(min_index, min_vzdalenost, color='red')  # Zvýraznění bodu
+        plt.text(min_index, min_vzdalenost, f' Min: {min_vzdalenost}', color='red', verticalalignment='bottom')
+
         plt.title(f'Vývoj celkové vzdálenosti v průběhu generací\nPopulace: {self.populace}, Mutace: {self.mutace}')
         plt.xlabel('Generace')
         plt.ylabel('Celková vzdálenost')
+        plt.legend()
         plt.grid(True)
         plt.show()
--- Genetic/AnalyzaVysledku.py
+++ Genetic/AnalyzaVysledku.py
@@ -3,22 +3,28 @@ from matplotlib import pyplot as plt
 
 class AnalizaVysledku:
     def __init__(self, populace, mutace):
-        # Inicializace objektu pro analýzu výsledků
         self.vzdalenosti = []  # Seznam pro ukládání vzdáleností v každé generaci
         self.populace = populace  # Velikost populace pro účely analýzy
         self.mutace = mutace  # Pravděpodobnost mutace pro účely analýzy
 
     def zaznamenej_vysledek(self, vzdalenost):
-        # Zaznamenání výsledku pro aktuální generaci
         self.vzdalenosti.append(vzdalenost)
 
     def vykresli_genetic_graf(self):
-        # Vykreslení grafu vývoje vzdálenosti v průběhu generací
         generace = range(1, len(self.vzdalenosti) + 1)
+        plt.plot(generace, self.vzdalenosti, label='Vzdálenost')
 
-        plt.plot(generace, self.vzdalenosti)
+        # Najít minimum a jeho index
+        min_vzdalenost = min(self.vzdalenosti)
+        min_index = self.vzdalenosti.index(min_vzdalenost) + 1  # +1 protože indexace generací začíná od 1
+
+        # Zvýraznit minimum
+        plt.scatter(min_index, min_vzdalenost, color='red')  # Zvýraznění bodu
+        plt.text(min_index, min_vzdalenost, f' Min: {min_vzdalenost}', color='red', verticalalignment='bottom')
+
         plt.title(f'Vývoj celkové vzdálenosti v průběhu generací\nPopulace: {self.populace}, Mutace: {self.mutace}')
         plt.xlabel('Generace')
         plt.ylabel('Celková vzdálenost')
-        plt.grid(True)  # Přidá mřížku pro lepší čitelnost
+        plt.legend()
+        plt.grid(True)
         plt.show()
