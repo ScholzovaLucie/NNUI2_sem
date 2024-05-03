6ea45d294209d0b25cc4ed4d8af382fde06cc870
ScholzovaLucie <lucka.scholz@gmail.com>
2024-05-03 10:06:58 +0200

    update main

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from AntColony.AntColony import AntColony
 from Genetic.GeneticAlgorithm import GenetickyAlgoritmus
+from Hopfield.HopfieldNetwork import HopfieldNetwork
 from Loader.DataLoader import DataLoader
 from PubsManager.SpravceHospod import SpravceHospod
 import openpyxl
 
 
-def main():
-    # Nastavení názvu souboru
-    file_name = "Pubs.xlsx"
-
-    # Načtení dat a formátování pro algoritmus
+def load_data(file_name):
     data_loader = DataLoader(file_name)
-    pubs_data = data_loader.format_data_for_algorithm()
+    return data_loader.format_data_for_algorithm()
 
+
+def create_distance_matrix(pubs_data):
     spravce_hospod = SpravceHospod(pubs_data)
+    return spravce_hospod.create_distance_matrix(pubs_data)
 
-    # Inicializace genetického algoritmu
+
+def run_hopfield_network(distance_matrix):
+    iterations = 10
+    network = HopfieldNetwork(distance_matrix)
+    hopfield_solution = network.solve(iterations)
+    network.print_hopfield_results(hopfield_solution, "Výsledky Hopfieldovy sítě:")
+
+
+def run_genetic_algorithm(pubs_data, spravce_hospod):
     geneticky_algoritmus = GenetickyAlgoritmus(
         pubs_data,
         spravce_hospod,
@@ -23,29 +31,27 @@ def main():
         generace=500,
         mutace_pravdepodobnost=0.1
     )
-
-    # Spuštění genetického algoritmu
     nejlepsi_cesta, celkova_vzdalenost = geneticky_algoritmus.geneticky_algoritmus()
+    geneticky_algoritmus.print_genetic_results(nejlepsi_cesta, celkova_vzdalenost, "Výsledky genetického algoritmu:")
 
-    # Výpis výsledků
-    print_results(nejlepsi_cesta, celkova_vzdalenost)
 
+def run_ant_colony(pubs_data):
     colony = AntColony(pubs_data, num_ants=5, num_iterations=20, alpha=1, beta=2, evaporation_rate=0.5)
     best_tour, best_distance = colony.run()
-    # Výpis výsledků
-    print_results(best_tour, best_distance)
+    colony.print_ant_colony_results(best_tour, best_distance, "Výsledky algoritmu mravenčí kolonie:")
 
 
-def print_results(nejlepsi_cesta, celkova_vzdalenost):
-    # Výpis nejlepšího pořadí hospod a celkové uražené vzdálenosti
-    print("Nejlepší pořadí návštěvy hospod:")
+def main():
+    file_name = "Pubs.xlsx"
+    pubs_data = load_data(file_name)
+    spravce_hospod = SpravceHospod(pubs_data)
+    distance_matrix = create_distance_matrix(pubs_data)
 
-    for hospoda in nejlepsi_cesta:
-        print(f"{hospoda['id']}. {hospoda['název']}")
-
-    print("Celková uražená vzdálenost:", celkova_vzdalenost)
+    # Spuštění algoritmů
+    run_hopfield_network(distance_matrix)
+    run_genetic_algorithm(pubs_data, spravce_hospod)
+    run_ant_colony(pubs_data)
 
 
 if __name__ == '__main__':
-    # Volání hlavní funkce
     main()
