7ef89f10629f8fe4cff5c7c9cedd029373683ae4
ScholzovaLucie <lucka.scholz@gmail.com>
2024-05-03 08:45:40 +0200

    přidání mravenčí kolonie

--- AntColony/AnalyzaVysledku.py
+++ AntColony/AnalyzaVysledku.py
@@ -3,22 +3,18 @@ from matplotlib import pyplot as plt
 
 class AnalizaVysledku:
     def __init__(self, populace, mutace):
-        # Inicializace objektu pro analýzu výsledků
-        self.vzdalenosti = []  # Seznam pro ukládání vzdáleností v každé generaci
-        self.populace = populace  # Velikost populace pro účely analýzy
-        self.mutace = mutace  # Pravděpodobnost mutace pro účely analýzy
+        self.vzdalenosti = []
+        self.populace = populace
+        self.mutace = mutace
 
     def zaznamenej_vysledek(self, vzdalenost):
-        # Zaznamenání výsledku pro aktuální generaci
         self.vzdalenosti.append(vzdalenost)
 
     def vykresli_graf(self):
-        # Vykreslení grafu vývoje vzdálenosti v průběhu generací
         generace = range(1, len(self.vzdalenosti) + 1)
-
         plt.plot(generace, self.vzdalenosti)
         plt.title(f'Vývoj celkové vzdálenosti v průběhu generací\nPopulace: {self.populace}, Mutace: {self.mutace}')
         plt.xlabel('Generace')
         plt.ylabel('Celková vzdálenost')
-        plt.grid(True)  # Přidá mřížku pro lepší čitelnost
+        plt.grid(True)
         plt.show()
